f5e23624b2d017af0fd111a5803a18e37d276ab6
Nahid-Hasan-Nobil <135527374+Nahid-Hasan-Nobil@users.noreply.github.com>
2025-03-11 20:55:21 +0600

    Update aawait.js

--- aawait.js
+++ aawait.js
@@ -3,12 +3,12 @@ console.log("First");
 async function a() { 
     await new Promise((resolve) => {
         setTimeout(() => {
-            let sum = 0;  // Initialize sum
+            let sum = 0;  
             for (let i = 0; i < 100000000; i++) {
                 sum += i;
             }
             console.log(sum);
-            resolve();  // Resolve the promise after execution
+            resolve();  
         }, 2000);
     });
 }
